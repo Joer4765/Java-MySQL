72ff7a8704378627fefb17b0a3c24264247091d0
Joer4765 <joer.tuhn@gmail.com>
2024-01-05 13:42:13 +0200

    init

---
+++ New Text Document.bat
@@ -0,0 +1,11 @@
+echo "# Java-MySQL" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Joer4765/Java-MySQL.git
+git push -u origin main
+git add .
+git commit -am "init"
+git push
+pause
